7997dbec56803c707121227cd7d1b4fbdf78eba5
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2024-12-04 16:39:32 +0100

    Fin Paso 6

--- tests/Feature/PageHomeTest.php
+++ tests/Feature/PageHomeTest.php
@@ -2,15 +2,19 @@
 
 use App\Models\Course;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Carbon;
 use function Pest\Laravel\get;
 
 uses(RefreshDatabase::class);
 
 it('shows courses overview', function () {
     // Arrange
-    Course::factory()->create(['title' => 'Course A', 'description' => 'Description Course A']);
-    Course::factory()->create(['title' => 'Course B', 'description' => 'Description Course B']);
-    Course::factory()->create(['title' => 'Course C', 'description' => 'Description Course C']);
+    Course::factory()->create(['title' => 'Course A', 'description' => 'Description Course A',
+        'released_at' => Carbon::now()]);
+    Course::factory()->create(['title' => 'Course B', 'description' => 'Description Course B',
+        'released_at' => Carbon::now()]);
+    Course::factory()->create(['title' => 'Course C', 'description' => 'Description Course C',
+        'released_at' => Carbon::now()]);
 
     // Act
     get(route('home'))
@@ -26,19 +30,30 @@ it('shows courses overview', function () {
 
 it('shows only released courses', function () {
     // Arrange
+    Course::factory()->create(['title' => 'Course A', 'released_at' => Carbon::yesterday()]);
+    Course::factory()->create(['title' => 'Course B']);
 
     // Act
-
-    // Assert
-
+    get(route('home'))
+        ->assertSeeText([
+            'Course A',
+        ])
+        ->assertDontSeeText([
+            'Course B',
+        ]);
 });
 
 it('shows courses by release date', function () {
     // Arrange
+    Course::factory()->create(['title' => 'Course A', 'released_at' => Carbon::yesterday()]);
+    Course::factory()->create(['title' => 'Course B', 'released_at' => Carbon::now()]);
 
     // Act
-
-    // Assert
+    get(route('home'))
+        ->assertSeeTextInOrder([
+            'Course B',
+            'Course A',
+        ]);
 
 });
 
